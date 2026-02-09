7acfa5a6ed617ec83dacdff3748274bc6d811508
Badmasri <badmasri14@gmail.com>
2026-02-09 10:11:30 +0530

    Add files via upload

---
+++ Java program/Pattern.java
@@ -0,0 +1,35 @@
+package src;
+
+public class Pattern
+{
+	public static void main(String[] args) 
+	{
+		StarPattern s =new StarPattern();
+	    System.out.println("Horizontal Pattern:");
+        s.horizontalStars();
+
+        System.out.println("\nVertical Pattern:");
+        s.verticalStars();
+    }
+}
+class StarPattern {
+
+ void horizontalStars() {
+     int n = 5;
+     for (int i = 1; i <= n; i++) {
+         System.out.print("* ");
+     }
+ }
+
+ void verticalStars() {
+     int n = 5;
+     for (int i = 1; i <= n; i++) {
+         System.out.println("*");
+     }
+ }
+}
+
+               
+   
+
+

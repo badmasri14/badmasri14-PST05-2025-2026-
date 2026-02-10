86652a42db1802d199ed7ee418f0756445b5ac49
Badmasri <badmasri14@gmail.com>
2026-02-10 14:08:17 +0530

    Add files via upload

---
+++ Java program/string.java
@@ -0,0 +1,14 @@
+package src;
+
+public class string {
+	public static void main(String[] args) {
+		        String str = "123";
+		        int num = Integer.parseInt(str);
+		        System.out.println("String value  : " + str);
+		        System.out.println("Integer value : " + num);
+		    }
+		}
+
+
+
+	

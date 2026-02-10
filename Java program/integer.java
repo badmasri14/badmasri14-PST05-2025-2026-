9611114a0b279084612e9f5167140dce474b054d
Badmasri <badmasri14@gmail.com>
2026-02-10 14:07:46 +0530

    Add files via upload

---
+++ Java program/integer.java
@@ -0,0 +1,13 @@
+package src;
+
+public class integer {
+
+    public static void main(String[] args) {
+
+        int num = 77;
+        String str = Integer.toString(num);
+
+        System.out.println("String value: " + str);
+    }
+}
+

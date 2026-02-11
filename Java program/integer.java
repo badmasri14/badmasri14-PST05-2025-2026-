f894edea362a03a5aae3d128c33e362c9be69efa
Badmasri <badmasri14@gmail.com>
2026-02-11 11:27:49 +0530

    Add files via upload

--- Java program/integer.java
+++ Java program/integer.java
@@ -1,13 +1,26 @@
 package src;
 
 public class integer {
-
     public static void main(String[] args) {
-
-        int num = 77;
-        String str = Integer.toString(num);
-
-        System.out.println("String value: " + str);
+        int num = 1234;
+        System.out.println("Integer: " + num);
+        IntToString obj = new IntToString();
+        obj.convert();
     }
 }
+class IntToString {
+	public void convert() {
+		int num = 1234;
+		String str = "";
+        while (num > 0) {
+            int digit = num % 10;
+            char ch = (char) (digit + '0'); 
+            str = ch + str;
+            num = num / 10;
+        }
+        System.out.println("String: " + str);
+    }
+}
+
+    	
 
